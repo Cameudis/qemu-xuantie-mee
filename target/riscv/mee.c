262b8f3b1f4187dab2559b2e32f56722ea946750
Ordirehv <1276837459@qq.com>
2025-03-26 15:23:14 +0800

    ctr implemented

--- target/riscv/mee.c
+++ target/riscv/mee.c
@@ -36,10 +36,14 @@
 
 /* BASICs */
 
-#define CACHE_LINE_LOG2 4
+#define CACHE_LINE_LOG2 6
 #define CACHE_LINE_SIZE (1 << CACHE_LINE_LOG2)
 #define CACHE_LINE_MASK (CACHE_LINE_SIZE - 1)
-#define MAC_BLOCK_SIZE 2
+#define AES_BLOCK_SIZE 16
+#define AES_BLOCK_NUM (CACHE_LINE_SIZE / AES_BLOCK_SIZE)
+#define MAC_BLOCK_NUM_LOG2 3
+#define MAC_BLOCK_NUM (1 << MAC_BLOCK_NUM_LOG2)
+
 // x^64 + x^4 + x^3 + x + 1
 #define POLY 0x1B
 
@@ -50,20 +54,22 @@ uint64_t meexc_start = 0;
 uint64_t meexc_len = 0;
 
 AES_KEY enc_key;
-AES_KEY dec_key;
-uint64_t hash_key[MAC_BLOCK_SIZE] = {0xb6db5eb0283229ae, 0x249325f2909d5fd0};
+// AES_KEY dec_key;
+uint64_t hash_key[MAC_BLOCK_NUM];
 
 typedef uint8_t Block __attribute__((vector_size(16)));
 // tweak_keys[Tweak index] = key
-static Block tweak_keys[32];
+// static Block tweak_keys[32];
 
 // PAT related structures
 #define PAT_LEVELS 4 // Number of levels in the PAT
 #define PAT_BLOCK_SIZE CACHE_LINE_SIZE
-#define PAT_CHILDREN_PER_NODE 2 // Number of children per node
-#define PAT_CHILDREN_PER_NODE_LOG2 1
+#define PAT_CHILDREN_PER_NODE_LOG2 MAC_BLOCK_NUM_LOG2
+#define PAT_CHILDREN_PER_NODE MAC_BLOCK_NUM  // Number of children per node
 #define PAT_CHILDREN_MASK (PAT_CHILDREN_PER_NODE - 1)
 #define PAT_N_INIT 1 // Initial value of the PAT
+#define align(addr) (addr & (~PAT_CHILDREN_MASK))
+
 typedef struct {
     uint64_t *tag;
     uint64_t *counter;
@@ -73,6 +79,7 @@ static uint64_t *pat_vers = NULL;
 static PAT_LEVEL pat_layout[PAT_LEVELS] = {};
 static uint64_t *pd_tags = NULL;
 
+/*
 static __attribute__((hot)) void apply_tweak(uint8_t *buf, uint64_t pa) {
     pa >>= CACHE_LINE_LOG2;
     for (size_t i = 0; i < sizeof(tweak_keys) / sizeof(tweak_keys[0]); ++i) {
@@ -81,6 +88,7 @@ static __attribute__((hot)) void apply_tweak(uint8_t *buf, uint64_t pa) {
         }
     }
 }
+*/
 
 static void debug_print(const char *msg, uint64_t pa, uint64_t val[]) {
 #ifdef PAT_DEBUG
@@ -106,13 +114,10 @@ static uint64_t increment_counter(uint64_t counter) {
 static void init_pat_vers(size_t size) {
     pat_vers = calloc(size / CACHE_LINE_SIZE, sizeof(*pat_vers));
     pd_tags = calloc(size / CACHE_LINE_SIZE, sizeof(*pd_tags));
-    assert(pat_vers);
-    assert(pd_tags);
+
     for (size_t i = 0; i < PAT_LEVELS; ++i) {
         pat_layout[i].tag = calloc(size >> (CACHE_LINE_LOG2 + (i + 1) * PAT_CHILDREN_PER_NODE_LOG2), sizeof(*(pat_layout[i].tag)));
-        assert(pat_layout[i].tag);
         pat_layout[i].counter = calloc(size >> (CACHE_LINE_LOG2 + (i + 1) * PAT_CHILDREN_PER_NODE_LOG2), sizeof(*(pat_layout[i].counter)));
-        assert(pat_layout[i].counter);
     }
 
     PA_END = PA_BASE + size;
@@ -148,10 +153,6 @@ static uint64_t get_pat_pos(size_t level, uint64_t off) {
     return off >> (CACHE_LINE_LOG2 + (level + 1) * PAT_CHILDREN_PER_NODE_LOG2);
 }
 
-static uint64_t align(uint64_t addr) {
-    return addr & (~PAT_CHILDREN_MASK);
-}
-
 static uint64_t mult_GF(uint64_t a, uint64_t b) {
     uint64_t result = 0;
     for (int i = 0; i < 64; ++i) {
@@ -166,15 +167,15 @@ static uint64_t mult_GF(uint64_t a, uint64_t b) {
 }
 
 static uint64_t calc_MAC(uint64_t *child, uint64_t parent, uint64_t pa) {
-    uint8_t nonce[CACHE_LINE_SIZE] = {};
-    uint8_t encrypted[CACHE_LINE_SIZE] = {};
+    uint8_t nonce[AES_BLOCK_SIZE] = {};
+    uint8_t encrypted[AES_BLOCK_SIZE] = {};
 
     ((uint64_t *)nonce)[0] = parent;
     ((uint64_t *)nonce)[1] = pa;
     AES_encrypt(nonce, encrypted, &enc_key);
 
     uint64_t result = *((uint64_t *)encrypted);
-    for (size_t i = 0; i < MAC_BLOCK_SIZE; ++i)
+    for (size_t i = 0; i < MAC_BLOCK_NUM; ++i)
         result ^= mult_GF(hash_key[i], child[i]);
     return result & ((1ULL << 56) - 1);
 }
@@ -189,7 +190,7 @@ static bool verify_mac(uint8_t *data, uint64_t pa, uint64_t ver) {
     if (expected_mac != pd_tags[off >> CACHE_LINE_LOG2])
         return false;
 
-    expected_mac = calc_MAC(&pat_vers[align(off >> CACHE_LINE_LOG2)], pat_layout[0].counter[get_pat_pos(0, off)], pa & ~((1<<(CACHE_LINE_LOG2+PAT_CHILDREN_PER_NODE_LOG2))-1));
+    expected_mac = calc_MAC(&pat_vers[align(off >> CACHE_LINE_LOG2)], pat_layout[0].counter[get_pat_pos(0, off)], pa & ~((1 << (CACHE_LINE_LOG2 + PAT_CHILDREN_PER_NODE_LOG2)) - 1));
     if (expected_mac != pat_layout[0].tag[get_pat_pos(0, off)]) {
         fprintf(stderr, "pa: 0x%lx, off: 0x%lx\n", pa & ~((1<<(CACHE_LINE_LOG2+PAT_CHILDREN_PER_NODE_LOG2))-1), off);
         fprintf(stderr, "pat_vers[align(off >> CACHE_LINE_LOG2)]: 0x%lx_%lx, pat_layout[0].counter[get_pat_pos(0, off)]: 0x%lx\n", pat_vers[align(off >> CACHE_LINE_LOG2)], pat_vers[align(off >> CACHE_LINE_LOG2)+1], pat_layout[0].counter[get_pat_pos(0, off)]);
@@ -198,8 +199,10 @@ static bool verify_mac(uint8_t *data, uint64_t pa, uint64_t ver) {
         return false;
     }
     for (size_t i = 1; i < PAT_LEVELS; ++i) {
-        expected_mac = calc_MAC(&pat_layout[i - 1].counter[align(get_pat_pos(i - 1, off))], pat_layout[i].counter[get_pat_pos(i, off)], 0);
-        if (expected_mac != pat_layout[i].tag[get_pat_pos(i, off)])
+        uint64_t pos_children = align(get_pat_pos(i - 1, off));
+        uint64_t pos_parent = get_pat_pos(i, off);
+        expected_mac = calc_MAC(&pat_layout[i - 1].counter[pos_children], pat_layout[i].counter[pos_parent], pos_children);
+        if (expected_mac != pat_layout[i].tag[pos_parent])
             return false;
     }
     return true;
@@ -217,13 +220,30 @@ static void update_mac(uint8_t *data, uint64_t pa, uint64_t ver) {
     for (size_t i = 0; i < PAT_LEVELS; ++i)
         update_counter(i, pa);
 
-    pat_layout[0].tag[get_pat_pos(0, off)] = calc_MAC(&pat_vers[align(off >> CACHE_LINE_LOG2)], pat_layout[0].counter[get_pat_pos(0, off)], pa & ~((1<<(CACHE_LINE_LOG2+PAT_CHILDREN_PER_NODE_LOG2))-1));
-    for (size_t i = 1; i < PAT_LEVELS; ++i)
-        pat_layout[i].tag[get_pat_pos(i, off)] = calc_MAC(&pat_layout[i - 1].counter[align(get_pat_pos(i - 1, off))], pat_layout[i].counter[get_pat_pos(i, off)], 0);
+    pat_layout[0].tag[get_pat_pos(0, off)] = calc_MAC(&pat_vers[align(off >> CACHE_LINE_LOG2)], pat_layout[0].counter[get_pat_pos(0, off)], pa & ~((1 << (CACHE_LINE_LOG2 + PAT_CHILDREN_PER_NODE_LOG2)) - 1));
+    for (size_t i = 1; i < PAT_LEVELS; ++i) {
+        uint64_t pos_children = align(get_pat_pos(i - 1, off));
+        uint64_t pos_parent = get_pat_pos(i, off);
+        pat_layout[i].tag[pos_parent] = calc_MAC(&pat_layout[i - 1].counter[pos_children], pat_layout[i].counter[pos_parent], pos_children);
+    }
     return;
 }
 
-static void decrypt_block(uint8_t *dest, uint8_t *src, uint64_t pa) {
+static void process_block(uint8_t *dest, uint8_t *src, uint64_t pa, uint64_t ver) {
+    Block nonce;
+    Block encrypted;
+
+    ((uint64_t *)&nonce)[0] = ver;
+    ((uint64_t *)&nonce)[1] = pa;
+    AES_encrypt((uint8_t *)&nonce, (uint8_t *)&encrypted, &enc_key);
+    *((Block *)dest) = *((Block *)src) ^ encrypted;
+
+    // apply_tweak(dest, pa);
+    // debug_print("d", pa, (uint64_t *)dest);
+    return;
+}
+
+static void decrypt_CTR(uint8_t *dest, uint8_t *src, uint64_t pa) {
     bool success = false;
 
     uint64_t ver = get_pat_ver(pa, &success);
@@ -235,8 +255,9 @@ static void decrypt_block(uint8_t *dest, uint8_t *src, uint64_t pa) {
 
     if (ver != PAT_N_INIT) { // PAT verification passed, decrypt
         // TODO
-        AES_decrypt(src, dest, &dec_key);
-        apply_tweak(dest, pa);
+        for (size_t i = 0; i < AES_BLOCK_NUM; ++i) {
+            process_block(dest + i * AES_BLOCK_SIZE, src + i * AES_BLOCK_SIZE, pa + i * AES_BLOCK_SIZE, ver);
+        }
         debug_print("d", pa, (uint64_t *)dest);
     } else { // PAT verification passed, but ver is n_init
         memcpy(dest, src, CACHE_LINE_SIZE);
@@ -248,17 +269,12 @@ verify_failed:
     assert(0);
 }
 
-static void encrypt_block(uint8_t *dest, uint8_t *src, uint64_t pa) {
-    uint8_t tmp_buf[CACHE_LINE_SIZE];
-    debug_print("e", pa, (uint64_t *)src);
-
+static void encrypt_CTR(uint8_t *dest, uint8_t *src, uint64_t pa) {
     uint64_t ver = update_pat_ver(pa);
-
     // TODO
-    memcpy(tmp_buf, src, sizeof(tmp_buf));
-    apply_tweak(tmp_buf, pa);
-    AES_encrypt(tmp_buf, dest, &enc_key);
-
+    for (size_t i = 0; i < AES_BLOCK_NUM; ++i) {
+        process_block(dest + i * AES_BLOCK_SIZE, src + i * AES_BLOCK_SIZE, pa + i * AES_BLOCK_SIZE, ver);
+    }
     update_mac(dest, pa, ver);
 }
 
@@ -268,9 +284,13 @@ void init_mee(void) {
         abort();
     }
     AES_set_encrypt_key(key_buf, sizeof(key_buf) * 8, &enc_key);
-    AES_set_decrypt_key(key_buf, sizeof(key_buf) * 8, &dec_key);
-
+    // AES_set_decrypt_key(key_buf, sizeof(key_buf) * 8, &dec_key);
+    /*
     if (sizeof(tweak_keys) != (size_t)getrandom(tweak_keys, sizeof(tweak_keys), GRND_RANDOM)) {
+        abort();
+    }
+    */
+    if (sizeof(hash_key) != (size_t)getrandom(hash_key, sizeof(hash_key), GRND_RANDOM)) {
         abort();
     }
 
@@ -294,8 +314,8 @@ static uint64_t ram_load_ptr(void *ptr, ram_addr_t pa, size_t sz) {
 
     size_t off = (uintptr_t)ptr & CACHE_LINE_MASK;
 
-    assert(off + sz <= 16);
-    decrypt_block(&buf[0], base_ptr, base_pa);
+    assert(off + sz <= CACHE_LINE_SIZE);
+    decrypt_CTR(&buf[0], base_ptr, base_pa);
     memcpy(&result, &buf[off], sz);
     return result;
 }
@@ -340,7 +360,7 @@ uint64_t mee_load_pa(CPURISCVState *env, ram_addr_t pa, size_t sz) {
         uint8_t result[CACHE_LINE_SIZE];
         ram_addr_t base_pa = pa & ~CACHE_LINE_MASK;
         address_space_read(cs->as, base_pa, MEMTXATTRS_UNSPECIFIED, buf, CACHE_LINE_SIZE);
-        decrypt_block(result, buf, base_pa);
+        decrypt_CTR(result, buf, base_pa);
         return *(uint64_t *)(result + (pa & CACHE_LINE_MASK));
     }
 }
@@ -354,9 +374,9 @@ static void ram_store_ptr(void *ptr, ram_addr_t pa, uint64_t val, size_t sz) {
     size_t off = (uintptr_t)ptr & CACHE_LINE_MASK;
 
     assert(off + sz <= CACHE_LINE_SIZE);
-    decrypt_block(&buf[0], base_ptr, base_pa);
+    decrypt_CTR(&buf[0], base_ptr, base_pa);
     memcpy(&buf[off], &val, sz);
-    encrypt_block(base_ptr, &buf[0], base_pa);
+    encrypt_CTR(base_ptr, &buf[0], base_pa);
 }
 
 static void any_store_ptr(void *ptr, ram_addr_t pa, uint64_t val, size_t sz) {
@@ -396,9 +416,9 @@ void mee_store_pa(CPURISCVState *env, ram_addr_t pa, uint64_t val, size_t sz) {
         uint8_t result[CACHE_LINE_SIZE];
         ram_addr_t base_pa = pa & ~CACHE_LINE_MASK;
         address_space_read(cs->as, base_pa, MEMTXATTRS_UNSPECIFIED, buf, CACHE_LINE_SIZE);
-        decrypt_block(result, buf, base_pa);
+        decrypt_CTR(result, buf, base_pa);
         memcpy(result + (pa & CACHE_LINE_MASK), &val, sz);
-        encrypt_block(buf, result, base_pa);
+        encrypt_CTR(buf, result, base_pa);
         address_space_write(cs->as, base_pa, MEMTXATTRS_UNSPECIFIED, buf, CACHE_LINE_SIZE);
     }
 }
